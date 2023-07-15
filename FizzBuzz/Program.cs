df6dc5b43a71f3f419141ccd2d849838c2bf9edc
GarethPN-MadeTech <gareth.pitt-nash@madetech.com>
2023-07-15 10:13:01 +0100

    Updated the Program.cs to use the rules to print the result to the console and inspected output

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,12 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using FizzBuzz.Rules;
+
 namespace FizzBuzz;
 
 public static class Program
 {
     static public void Main(String[] args)
     {
-  
-        Console.WriteLine("Hello, World!");
+        var fizzRule = new FizzRule();
+        var buzzRule = new BuzzRule();
+        var fizzBuzzRule = new FizzBuzzRule();
+        
+        for (int i = 1; i < 100; i++)
+        {
+            if (fizzRule.Run(i))
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (buzzRule.Run(i))
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (fizzBuzzRule.Run(i))
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 }
