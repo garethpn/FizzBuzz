2da22709bde8e12d3dcf9db964cc257ddcbe89e8
GarethPN-MadeTech <gareth.pitt-nash@madetech.com>
2023-07-15 10:04:32 +0100

    Refactored FizzBuzzRuleTests as 2 methods where doing the same thing with different values, so merged into 1

--- FizzBuzz.UnitTests/FizzBuzzRuleTests.cs
+++ FizzBuzz.UnitTests/FizzBuzzRuleTests.cs
@@ -9,20 +9,10 @@ public class FizzBuzzRuleTests
     [TestCase(3)]
     [TestCase(6)]
     [TestCase(9)]
-    public void Number_Divisible_By_3_Returns_False_For_FizzBuzz(int value)
-    {
-        var fizzBuzzRule = new FizzBuzzRule();
-
-        var shouldPrintFizzBuzz = fizzBuzzRule.Run(value);
-
-        Assert.IsFalse(shouldPrintFizzBuzz);
-    }
-    
-    [Test]
     [TestCase(5)]
     [TestCase(10)]
     [TestCase(20)]
-    public void Number_Divisible_By_5_Returns_False_For_FizzBuzz(int value)
+    public void Number_Divisible_By_3_only_or_5_only_Returns_False_For_FizzBuzz(int value)
     {
         var fizzBuzzRule = new FizzBuzzRule();
 
@@ -35,7 +25,7 @@ public class FizzBuzzRuleTests
     [TestCase(15)]
     [TestCase(30)]
     [TestCase(45)]
-    public void Number_Divisible_By_3_And_5_Returns_True_For_FizzBuzz(int value)
+    public void Number_Divisible_By_Both_3_And_5_Returns_True_For_FizzBuzz(int value)
     {
         var fizzBuzzRule = new FizzBuzzRule();
 
